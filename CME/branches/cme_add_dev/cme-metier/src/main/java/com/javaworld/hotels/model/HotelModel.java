7195f0d7708811cc224ba169254e0464fdbc1113
emmanuel.agius@valtech.fr <emmanuel.agius@valtech.fr@f5e6b2d8-20d6-a661-e151-885440e017b0>
2013-09-12 23:52:52 +0000

    EAG : - ajoute les Keywords sur l'ensemble du projet CME - Plus de la classe DAO pour la gestion Hotels et Villes - mise à jour des commentaires

--- CME/branches/cme_add_dev/cme-metier/src/main/java/com/javaworld/hotels/model/HotelModel.java
+++ CME/branches/cme_add_dev/cme-metier/src/main/java/com/javaworld/hotels/model/HotelModel.java
@@ -1,12 +1,10 @@
 // //////////////////////////////////////////////////////////////////////////////
 // HotelModel: $Source$
-// TODO Class summary goes here
 //
 // Created : 15 nov. 2005 by jfsmart
 // Last modified $Date$ by $Author$
 // Revision: $Revision$
 // Version : $ID$
-// Copyright (c) 2005
 // //////////////////////////////////////////////////////////////////////////////
 
 package com.javaworld.hotels.model;
@@ -18,27 +16,15 @@ import com.javaworld.hotels.businessobjects.Hotel;
 import com.javaworld.hotels.dao.HotelDao;
 
 /**
- * Une classe simple proposant quelques services pour la base de données
- * d'hotels.
+ * Une classe simple proposant quelques services d'hotels.
  * @author jfsmart
  */
 public class HotelModel {
 
     /**
-     * La liste de toutes les villes connues dans la base de données.
-     */
-    private static String[] villes = { "Paris", "Londres" };
-
-    /**
      * La liste de tous les hotels de la base de données.
      */
-    private static Hotel[] hotels = {
-            new Hotel("Hotel Latin", "Quartier latin", "Paris", 3),
-            new Hotel("Hotel Etoile", "Place de l'Etoile", "Paris", 4),
-            new Hotel("Hotel Vendome", "Place Vendome", "Paris", 5),
-            new Hotel("Hotel Hilton", "Trafalgar Square", "Londres", 4),
-            new Hotel("Hotel Ibis", "The City", "Londres", 3), };
-
+    private HotelDao hotels = new HotelDao();
     /**
      * Retourne les hôtels dans une ville donnée.
      * @param ville le nom de la ville
@@ -46,7 +32,7 @@ public class HotelModel {
      */
     public List <Hotel> trouveHotelsParVille(String ville) {
         List <Hotel> hotelsTrouves = new ArrayList <Hotel>();
-        for (Hotel hotel : hotels) {
+        for (Hotel hotel : hotels.getHotels()) {
             if (hotel.getVille().equalsIgnoreCase(ville)) {
                 hotelsTrouves.add(hotel);
             }
@@ -59,6 +45,6 @@ public class HotelModel {
      * @return une liste des noms de villes
      */
     public String[] trouveVillesDiponibles() {
-        return villes;
+        return hotels.getVilles();
     }
 }
