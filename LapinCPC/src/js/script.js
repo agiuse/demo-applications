190a459f87193c8214e2a22276ffb4dbbe5173a8
emmanuel.agius <emmanuel.agius@valtech.fr>
2014-02-27 21:12:14 +0000

    EAG : Développez Couché Saison 3 - Leçon 1 - Oct. 2013

--- LapinCPC/src/js/script.js
+++ LapinCPC/src/js/script.js
@@ -1,12 +1,40 @@
+	var stage;
+	var preloadCount =0 ;
+	var PRELOADTOTAL = 1;  // nombre de ressources à charger
+
+	var img_joueur= new Image();
+	var obj_joueur;
+
+
 function startGame()
 {
-	var stage = new createjs.Stage(document.getElementById("gameCanvas"));
+	preloadAssets();
+}
 
-	var text = new createjs.Text("COIN COIN !", "36px Arial", "#777777");
+function preloadAssets()
+{
+	img_joueur.onload = preloadUpdate();
+	img_joueur.src = "images/joueur.png";
+}
 
-	stage.addChild(text);
-	text.x = 360;
-	text.y = 200;
+function preloadUpdate()
+{
+	preloadCount++;
+	if (preloadCount == PRELOADTOTAL) launchGame();
+}
 
+function launchGame()
+{
+	stage = new createjs.Stage(document.getElementById("gameCanvas"));
+	
+	obj_joueur = new createjs.Bitmap(img_joueur);
+	stage.addChild(obj_joueur);
+
+	createjs.Ticker.setFPS(30);
+	createjs.Ticker.addEventListener("tick", mainTick);
+}
+
+function mainTick()
+{
 	stage.update();
 }
