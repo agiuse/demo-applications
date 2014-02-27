0297c383984ee6ae40086f938d761a77941e36a9
emmanuel.agius <emmanuel.agius@valtech.fr>
2014-02-27 21:26:14 +0000

    EAG : Développez Couché Saison 3 - Leçon 3 - Nov. 2013 - ajout du ciel

--- LapinCPC/src/js/script.js
+++ LapinCPC/src/js/script.js
@@ -1,10 +1,12 @@
 	var stage;
 	var preloadCount =0 ;
-	var PRELOADTOTAL = 1;  // nombre de ressources à charger
+	var PRELOADTOTAL = 4;  // nombre de ressources à charger
 
 	var img_joueur= new Image();
 	var obj_joueur;
 
+	var img_sky = [ new Image(), new Image(), new Image() ];
+	var obj_sky = [];
 
 function startGame()
 {
@@ -15,6 +17,12 @@ function preloadAssets()
 {
 	img_joueur.onload = preloadUpdate();
 	img_joueur.src = "images/joueur.png";
+
+	for ( var i=0; i < 3; i++)
+	{
+		img_sky[i].onload = preloadUpdate();
+		img_sky[i].src = "images/ciel" + i + ".png";
+	}
 }
 
 function preloadUpdate()
@@ -27,6 +35,11 @@ function launchGame()
 {
 	stage = new createjs.Stage(document.getElementById("gameCanvas"));
 	
+	for ( var i=0; i < 3; i++)
+	{
+		obj_sky[i] = new createjs.Bitmap(img_sky[i]);
+		stage.addChild(obj_sky[i]);
+	}
 	obj_joueur = new createjs.Bitmap(img_joueur);
 	stage.addChild(obj_joueur);
 
