91d5e4b31eb42ce7e12f2bda943ad60fec541858
emmanuel.agius@valtech.fr <emmanuel.agius@valtech.fr@f5e6b2d8-20d6-a661-e151-885440e017b0>
2014-03-08 10:36:10 +0000

    EAG : Déplace le code de l'objet Saucisse dans le fichier saucisses.js

---
+++ LapinCPC/trunk/js/saucisses.js
@@ -0,0 +1,63 @@
+	var SAUCISSE_COUNT = 10;
+	var SAUCISSE_TYPE_NUMBER = 2;
+	var SAUCISSE_TYPE_POURRIE = 1;
+	var SAUCISSE_TYPE_BONNE = 0;
+	var img_saucisse = [ new Image(), new Image() ];
+	var obj_saucisse = [];
+
+// Chargement des ressources
+function preloadAssetsSaucisse()
+{
+	for ( var i=0; i < SAUCISSE_TYPE_NUMBER; i++)
+	{
+		img_saucisse[i].onload = preloadUpdate();
+		img_saucisse[i].src = "images/saucisse" + i + ".png";
+	}
+
+}
+
+// ============================================================================================================================
+// Definition du 'constructor' pour Saucisse.
+// C'est un peu comme si on créait une classe 'Saucisse' qui hérite de createjs.Bitmap
+function Saucisse() {
+	createjs.Bitmap.call(this);	// appel du 'constructor' parent (pas obligatoire mais recommandé)
+	this.preparerSaucisse();	// on appelle la méthode preparerSaucisse (pas obligatoire mais autant le faire de suite) 
+	this.vitesse = 4;
+}
+
+//Nécessaire afin que Saucisse hérite de createjs.Bitmap
+Saucisse.prototype = new createjs.Bitmap();		
+
+// Définition de la méthode preparerSaucisse pour la classe 'Saucisse'
+// permet de creer un saucisse au hasard a droite du canvas
+// permet de creer une saucisse bonne et de temps en temps une saucisse pourrie
+Saucisse.prototype.preparerSaucisse = function ()
+{
+	// this représente l'objet 'Saucisse' 
+	this.x = Math.floor(Math.random() * STAGE_HEIGHT + STAGE_WIDTH);
+	this.y = Math.floor(Math.random() * STAGE_HEIGHT);
+
+	this.pourrie = Math.random() < 0.5;
+
+	this.image =  (this.pourrie) ? img_saucisse[SAUCISSE_TYPE_POURRIE] : img_saucisse[SAUCISSE_TYPE_BONNE];
+}
+
+// 
+Saucisse.prototype.isCollision = function ( obj_right )
+{
+	return  (( this.x > obj_right.x - 40 ) &&
+		( this.x < obj_right.x + 96 ) &&
+		( this.y > obj_right.y - 16 ) &&
+		( this.y < obj_right.y + 44 )
+		);
+}
+
+Saucisse.prototype.isLeftStage = function ()
+{
+	return ( this.x < -PLAYER_HALF_WIDTH );
+}
+
+Saucisse.prototype.moveToLeft = function ()
+{
+	this.x -= this.vitesse;
+}
--- LapinCPC/trunk/js/script.js
+++ LapinCPC/trunk/js/script.js
@@ -2,6 +2,8 @@
 	var preloadCount =0 ;
 	var PRELOADTOTAL = 11;  // nombre de ressources à charger
 
+	var SAUCISSE_COUNT = 10;
+
 	var img_joueur= new Image();
 	var obj_joueur;
 
@@ -14,13 +16,6 @@
 	var STAGE_HEIGHT = 480;
 	var PLAYER_HALF_WIDTH = 64;
 	var PLAYER_HALF_HEIGHT = 32;
-
-	var SAUCISSE_COUNT = 10;
-	var SAUCISSE_TYPE_NUMBER = 2;
-	var SAUCISSE_TYPE_POURRIE = 1;
-	var SAUCISSE_TYPE_BONNE = 0;
-	var img_saucisse = [ new Image(), new Image() ];
-	var obj_saucisse = [];
 
 	var img_tir = new Image();
 	var obj_tir;
@@ -77,11 +72,7 @@ function preloadAssets()
 		img_sky[i].src = "images/ciel" + i + ".png";
 	}
 
-	for ( var i=0; i < SAUCISSE_TYPE_NUMBER; i++)
-	{
-		img_saucisse[i].onload = preloadUpdate();
-		img_saucisse[i].src = "images/saucisse" + i + ".png";
-	}
+	preloadAssetsSaucisse();
 
 	createjs.Sound.registerPlugins( [ createjs.WebAudioPlugin, createjs.HTMLAudioPlugin ] );
 	createjs.Sound.addEventListener( "loadComplete", preloadUpdate );
@@ -350,48 +341,3 @@ BonusLapin.prototype.isLeftStage = function ()
 	return ( this.x < -PLAYER_HALF_WIDTH );
 }
 
-// ============================================================================================================================
-// Definition du 'constructor' pour Saucisse.
-// C'est un peu comme si on créait une classe 'Saucisse' qui hérite de createjs.Bitmap
-function Saucisse() {
-	createjs.Bitmap.call(this);	// appel du 'constructor' parent (pas obligatoire mais recommandé)
-	this.preparerSaucisse();	// on appelle la méthode preparerSaucisse (pas obligatoire mais autant le faire de suite) 
-	this.vitesse = 4;
-}
-
-//Nécessaire afin que Saucisse hérite de createjs.Bitmap
-Saucisse.prototype = new createjs.Bitmap();		
-
-// Définition de la méthode preparerSaucisse pour la classe 'Saucisse'
-// permet de creer un saucisse au hasard a droite du canvas
-// permet de creer une saucisse bonne et de temps en temps une saucisse pourrie
-Saucisse.prototype.preparerSaucisse = function ()
-{
-	// this représente l'objet 'Saucisse' 
-	this.x = Math.floor(Math.random() * STAGE_HEIGHT + STAGE_WIDTH);
-	this.y = Math.floor(Math.random() * STAGE_HEIGHT);
-
-	this.pourrie = Math.random() < 0.5;
-
-	this.image =  (this.pourrie) ? img_saucisse[SAUCISSE_TYPE_POURRIE] : img_saucisse[SAUCISSE_TYPE_BONNE];
-}
-
-// 
-Saucisse.prototype.isCollision = function ( obj_right )
-{
-	return  (( this.x > obj_right.x - 40 ) &&
-		( this.x < obj_right.x + 96 ) &&
-		( this.y > obj_right.y - 16 ) &&
-		( this.y < obj_right.y + 44 )
-		);
-}
-
-Saucisse.prototype.isLeftStage = function ()
-{
-	return ( this.x < -PLAYER_HALF_WIDTH );
-}
-
-Saucisse.prototype.moveToLeft = function ()
-{
-	this.x -= this.vitesse;
-}
