8075d3f8115f5aa9bd511267591a9a11bcf61626
emmanuel.agius@valtech.fr <emmanuel.agius@valtech.fr@f5e6b2d8-20d6-a661-e151-885440e017b0>
2014-04-03 04:02:12 +0000

    EAG : validation de l'objet Model Saucisse et View Saucisse

--- LapinCPC/trunk/js/saucisses.js
+++ LapinCPC/trunk/js/saucisses.js
@@ -15,32 +15,40 @@ function preloadAssetsSaucisse()
 // ============================================================================================================================
 // Definition du 'constructor' pour Saucisse.
 // C'est un peu comme si on créait une classe 'Saucisse' qui hérite de createjs.Bitmap
-function ViewSaucisse(stage,img_saucisse) {
+function ViewSaucisse(stage,img_saucisse,name) {
 	createjs.Bitmap.call(this);	// appel du 'constructor' parent (pas obligatoire mais recommandé)
+	this.name = name;
 	this.stage=stage;
 	this.img_saucisse = img_saucisse;
 	this.stage.addChild(this);
-	this.preparerSaucisse();	// on appelle la méthode preparerSaucisse (pas obligatoire mais autant le faire de suite) 
+	console.log(this.name + " View saucisse is created!");
 }
 //Nécessaire afin que Saucisse hérite de createjs.Bitmap
 ViewSaucisse.prototype = new createjs.Bitmap();		
 
-ViewSaucisse.prototype.display = function (observable)
+ViewSaucisse.prototype.preparer = function (observable)
 { 
-	this.x = observable.getX();
-	this.y = observable.getY();
+	this.x = observable.x;
+	this.y = observable.y;
 	this.image =  (observable.pourrie) ? this.img_saucisse[1] : this.img_saucisse[0];
-
 	this.rotation = (Math.random() * 10) - 5 ;
+	console.log(this.name + " View saucisse is ready!");
 }
 
+ViewSaucisse.prototype.display = function (observable)
+{ 
+	this.x = observable.x;
+	console.debug(this.name + " View saucisse is displayed!");
+}
 
 // ============================================================================================================================
 // Definition du 'constructor' pour ModelSaucisse.
 function ModelSaucisse(observer) {
 	this.observer = observer;
+	this.name = observer.name;
 	this.vitesse = 4;
 	this.preparerSaucisse();	// on appelle la méthode preparerSaucisse (pas obligatoire mais autant le faire de suite) 
+	console.log(this.name + " Model saucisse is created!");
 }
 
 // Définition de la méthode preparerSaucisse pour la classe 'ModelSaucisse'
@@ -52,9 +60,9 @@ ModelSaucisse.prototype.preparerSaucisse = function ()
 	// this représente l'objet 'Saucisse' 
 	this.x = Math.floor(Math.random() * STAGE_HEIGHT + STAGE_WIDTH);
 	this.y = Math.floor(Math.random() * STAGE_HEIGHT);
-
 	this.pourrie = Math.random() < 0.5;
-this.observer.display(this);
+	this.observer.preparer(this);
+	console.log(this.name + " Model saucisse is ready!");
 }
 
 // 
