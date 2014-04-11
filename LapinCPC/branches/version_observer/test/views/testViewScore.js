7f5e55c8a56312eeccb858500b6f981ff83a6289
emmanuel.agius <emmanuel.agius@valtech.fr>
2014-04-11 18:34:11 +0000

    EAG : Remis la méthode dec dans l'objet Score.

---
+++ LapinCPC/branches/version_observer/test/views/testViewScore.js
@@ -0,0 +1,65 @@
+"use strict;"
+// ====================================================================
+// objet simulant l'observable observé par l'objet testé ViewScore
+// Dans le cadre des tests, cet objet est directement un observable et ne possède pas un Model propre.
+// Comme par exemple l'observable vie ou coordonnee dans l'objet ModelPlayer
+function ObjetScore(name)
+{
+	this.name = name;	
+	Observable.call(this, name);
+	
+	console.log(this.name, "Constructeur ObjetScore");
+	this.preparer();
+}
+
+ObjetScore.prototype = new Observable();
+
+ObjetScore.prototype.preparer = function()
+{
+	this.score=10;
+	this.notify('prepare');
+}
+
+ObjetScore.prototype.run = function(valeur)
+{
+	this.score = valeur;
+	console.log(this.name, "ObjetScore : traitement de la nouvelle valeur = ", this.score);
+	this.notify('display');
+}
+
+ObjetScore.prototype.get = function()
+{
+	return this.score;
+}
+
+// ====================================================================
+// -----------------------------------------------------------------
+function test1() {
+	console.log("Test 1 : Affichage du score");
+
+	var obj_stage = new createjs.Stage(document.getElementById("gameCanvas"));
+
+	var obj_controller_score = new ControllerScore(obj_stage, 'controller_score_1');
+	var obj_observable = new ObjetScore('observable');
+	
+	console.log("value de ",obj_observable.name, " = ", obj_observable.get());
+
+	obj_controller_score.toObserve(obj_observable);
+	obj_stage.update();
+	
+	obj_observable.run(14);
+	obj_stage.update();
+
+	obj_observable.run(3);
+	obj_stage.update();
+
+}
+
+
+function startTest() {
+
+	console.clear();
+
+	test1();
+}
+
