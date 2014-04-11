407c0a0691340b7e7f45acfd1fbc4b1c18ed071e
emmanuel.agius <emmanuel.agius@valtech.fr>
2014-04-11 18:24:12 +0000

    EAG : * Ajout des tests Score et MVC Score * Reorganisation des tests

--- LapinCPC/branches/version_observer/test/views/testViewLife.js
+++ LapinCPC/branches/version_observer/test/views/testViewLife.js
@@ -3,12 +3,10 @@
 // objet simulant l'observable observé par l'objet testé ViewLife
 // Cet objet est directement un observable et n'est pas un Model
 // Comme par exemple l'observable vie ou coordonnee dans l'objet ModelPlayer
-function ObjetVie(name, obj_controller_vies)
+function ObjetVie(name)
 {
 	this.name = name;	
 	Observable.call(this, name);
-
-	this.add( obj_controller_vies.getRefObjectView() );
 	
 	console.log(this.name, "Constructeur ObjetVie");
 	this.preparer();
@@ -42,7 +40,8 @@ function test1() {
 	obj_stage = new createjs.Stage(document.getElementById("gameCanvas"));
 
 	obj_controller_vies = new ControllerLife(obj_stage, 'controller_life_1');
-	obj_observable = new ObjetVie('observable', obj_controller_vies);
+	obj_observable = new ObjetVie('observable');
+	obj_controller_vies.toObserve(obj_observable);
 		
 	console.log("value de ",obj_observable.name, " = ", obj_observable.get());
 
