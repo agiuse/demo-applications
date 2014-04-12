04a674acb8c3fbf3a9f1b7c339d2836f1b5eeca1
emmanuel.agius <emmanuel.agius@valtech.fr>
2014-04-12 09:08:50 +0000

    EAG : Ajout les schémas UML + commentaire

--- LapinCPC/branches/version_observer/js/highscore.js
+++ LapinCPC/branches/version_observer/js/highscore.js
@@ -4,6 +4,26 @@
 // L'objet ViewScore s'occupe de l'affichage du Score
 // Cet objet observe l'observable Score
 // ============================================================================================================================
+/*
+@startuml
+title Class <b>ViewScore<b>
+class createjs.Text
+
+class ViewScore {
+	createjs.Stage stage
+	String name
+	--
+	int x = 8
+	int y = 450
+	Boolean visible=true
+	==
+	__ notified __
+	display(obj_observable)
+}
+
+createjs.Text <|-- ViewScore
+@enduml
+*/
 function ViewScore(stage, name)
 {
 	createjs.Text.call(this, "Score : 0", "24px Arial", "#000000" );
@@ -26,8 +46,23 @@ ViewScore.prototype.display = function(obj_observable)
 }
 
 // ============================================================================================================================
-// Constructeur de controller Score
+// Class ControllerScore
 // ============================================================================================================================
+/*
+@startuml
+title Class <b>ControllerScore</b>
+class ViewScore
+
+class ControllerScore {
+	createjs.Stage stage
+	String name
+	==
+	ViewScore getObserver()
+}
+
+createText <|-- ViewScore
+@enduml
+*/
 function ControllerScore(obj_stage, name)
 {
 	this.obj_stage = obj_stage;
@@ -35,7 +70,154 @@ function ControllerScore(obj_stage, name)
 	this.obj_view_score = new ViewScore(this.obj_stage, this.name);
 }
 
-ControllerScore.prototype.toObserve = function(obj_observable)
+// Renvoie la référence de l'objet observeur géré par le Controller
+ControllerScore.prototype.getObserver = function()
 {
-	obj_observable.add( this.obj_view_score ); // L'objet ViewScore est en observation de type Score
+	return this.obj_view_score;
+}
+
+// ============================================================================================================================
+// MVC HighScore
+// ============================================================================================================================
+// Classe ViewHighScore
+// Cette classe s'occupe de l'affichage du HighScore à l'écran.
+// Elle observe la classe ModelHighView.
+// ============================================================================================================================
+/*
+@startuml
+title Class <b>ViewHighScore</b>
+class createjs.Text
+
+class ViewScore {
+	createjs.Stage stage
+	String name
+	--
+	int x = 300
+	int y = 450
+	Boolean visible=true
+	==
+	__ notified __
+	display(obj_observable)
+}
+
+createjs.Text <|-- ViewScore
+@enduml
+*/
+function ViewHighScore(stage).
+{
+	createjs.Text.call(this, "Highscore : 0", "24px Arial", "#00000");
+	this.stage = stage;
+	this.x = 300;
+	this.y = 450;
+	this.stage.addChild(this);
+	this.visible=true;
+
+}
+
+//NÃ©cessaire afin que ViewHighScore hÃ©rite de createjs.Text
+ViewHighScore.prototype = new createjs.Text();
+
+ViewHighScore.prototype.display = function(obj_observable) {	// notifie lors du changement du HighScore
+	this.text = "Highscore : " + obj_observable.get();
+}
+
+// ============================================================================================================================
+// Classe ModelHighView
+// Cette classe gère la valeur du HighScore.
+// Cette classe gère le high score qui un observable de type Score
+// ============================================================================================================================
+/*
+@startuml
+title Class <b>ModelHighScore</b>
+
+class Score
+
+class ModelHighScore {
+	String name
+	==
+	int get()
+	set(int)
+}
+
+ModelHighScore *-- Score : nb_points
+@enduml
+*/
+function ModelHighScore(name)
+{
+	this.name = name;
+	console.log(this.name + " View is being created...");
+	
+	this.nb_points = new Score(this.name);
+	
+	console.log(this.name + " View is created!");
+
+}
+
+ModelHighScore.prototype.get = function()
+{
+	return this.nb_points.get();
+}
+
+ModelHighScore.prototype.set = function(nb_points)
+{
+	this.nb_points.init(nb_points);
+}
+
+// ============================================================================================================================
+// Classe Controller
+// Cette classe permet de gérer le MVC 
+// ============================================================================================================================
+/*
+@startuml
+title Class <b>ControllerScore</b>
+
+class ViewHighScore
+class ModelHighScore
+class Score
+
+class ControllerHighScore {
+	createjs.Stage stage
+	String name
+	==
+	ViewScore getObserver()
+}
+
+ControllerHighScore *-- ViewHighScore
+ControllerHighScore *-- ModelHighScore
+ModelHighScore *-- Score : nb_points
+Score .. ViewHighScore : "observable/observer"
+
+@enduml
+*/
+function ControllerHighScore(obj_stage, name)
+{
+	this.obj_stage = obj_stage;
+	this.name = name;
+	console.log(this.name, " Controller is being created...");
+
+	this.obj_model_highscore = new ModelHighScore(this.name);
+	this.obj_model_highscore.nb_points.add(new ViewHighScore(this.obj_stage, this.name) ); // L'objet ViewHighScore est en observation du highscore
+	
+	console.log(this.name, " Controller is created...");
+}
+
+ControllerHighScore.prototype.prepare = function(nb_points)
+{
+	this.obj_model_highscore.set(nb_points);
+}
+
+// Verifie que le score ne depasse pas le highscore
+// Si oui le highscore change (Model) et la vue est notifiée du changement.
+ControllerHighScore.prototype.display = function(obj_observable)
+{ // notifie lors du changement du score du joueur
+	if (this.obj_observable.get() > this.obj_model_highscore.get() )
+		this.obj_model_highscore.set( observable.get() );
+}
+
+// Renvoie la référence de l'observer
+// Pour le HighScore ce n'est pas la Vue qui observe directement le score mais le controller
+// car le controller a un traitement pour mettre à jour le highScore
+ControllerHighScore.prototype.getObserver = function()
+{
+	return this;
 }
