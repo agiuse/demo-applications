d8dd63b70a4a5ab429e5511fba4a2a3065726183
emmanuel.agius <emmanuel.agius@valtech.fr>
2014-04-22 15:45:33 +0000

    EAG - Test Unitaires: * mise en place des tests unitaires sur le MVC Player * ajout la dimension de l'image dans createjs.Stage fake * relecture du code player.js

--- LapinCPC/branches/version_observer/test/lib/createjs-fake.js
+++ LapinCPC/branches/version_observer/test/lib/createjs-fake.js
@@ -1,6 +1,11 @@
 'use strict';
 
-var createjs={Stage: function Stage(){}, Text: function Text(text, police, color) {console.info('Text constructor :', text, police, color)}};
+var createjs={
+	Stage: function Stage(){ this.touches = {}; this.canvas= { width: 640, height: 480}; }, 
+	Text: function Text(){},
+	Bitmap: function Bitmap(){},
+	LoadQueue: function LoadQueue(){}
+};
 
 // ====================================================================
 createjs.Stage.prototype.addChild = function(obj_createjs)
@@ -8,5 +13,9 @@ createjs.Stage.prototype.addChild = function(obj_createjs)
 	console.info("addChild : ", obj_createjs);
 }
  
+createjs.LoadQueue.prototype.getResult = function(index)
+{
+	return { width: 128, height: 64 };
+}
 
 
