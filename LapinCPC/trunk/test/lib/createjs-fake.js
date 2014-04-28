258a3c11d42bb3f17ab250e3331027b5632a7fae
emmanuel.agius <emmanuel.agius@valtech.fr>
2014-04-28 13:33:46 +0000

    EAG - Espace Nom en Javascript : * mise en forme l'espace nom createjs

--- LapinCPC/trunk/test/lib/createjs-fake.js
+++ LapinCPC/trunk/test/lib/createjs-fake.js
@@ -1,33 +1,54 @@
 'use strict';
 
-var createjs={
-	Stage: function Stage(){ this.touches = {}; this.canvas= { width: 640, height: 480}; }, 
-	Text: function Text(){},
-	Bitmap: function Bitmap(){},
-	LoadQueue: function LoadQueue(){}
-};
+var createjs={};
 
-// ====================================================================
-createjs.Stage.prototype.addChild = function(obj_createjs)
+;( function()
 {
-	console.info("addChild : ", obj_createjs);
-}
- 
-createjs.LoadQueue.prototype.getResult = function(index)
-{
-	var elt;
-	switch (index) {
-	case 'player0':
-		elt = { width: 128, height: 64 };
-		break;
-	case 'bonne_saucisse':
-		elt = { width: 64, height: 32 };
-		break;
-	case 'mauvaise_saucisse':
-		elt = { width: 64, height: 32 };
-		break;
+	// ====================================================================
+	createjs.Stage = function()
+	{ 
+		this.touches = {};
+		this.canvas= { width: 640, height: 480};
 	}
-	return elt;
-}
+
+	createjs.Stage.prototype.addChild = function(obj_createjs)
+	{
+		console.info("addChild : ", obj_createjs);
+	}
+
+	// ====================================================================
+	createjs.Text = function()
+	{
+	}
+
+	// ====================================================================
+	createjs.Bitmap = function Bitmap()
+	{
+	}
+
+	// ====================================================================
+	createjs.LoadQueue = function LoadQueue(){
+	}
+
+	createjs.LoadQueue.prototype.getResult = function(index)
+	{
+		var elt;
+		switch (index) {
+		case 'player0':
+			elt = { width: 128, height: 64 };
+			break;
+		case 'bonne_saucisse':
+			elt = { width: 64, height: 32 };
+			break;
+		case 'mauvaise_saucisse':
+			elt = { width: 64, height: 32 };
+			break;
+		}
+		return elt;
+	}
+
+}());
 
 
+
+ 
